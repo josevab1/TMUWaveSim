c1a94606710eb1e608f0f595b08c06ac6b7b1b9a
tylerl126 <tyler.luu.1832@gmail.com>
2025-03-20 21:01:02 -0400

    basic movement #2

--- player.gd
+++ player.gd
@@ -11,8 +11,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var input := Vector3.ZERO
-	input.x = Input.get_axis("move_left","move_right")
-	input.z = Input.get_axis("move_forward","move_back")
+	input.x = Input.get_axis("ui_left","ui_right")
+	input.z = Input.get_axis("ui_up","ui_down")
 	apply_central_force(input * 1200.0 * delta)
 	if Input.is_action_just_pressed(("ui_cancel")):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
